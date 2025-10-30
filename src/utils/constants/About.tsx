d04d2ff0bd61fcfb226d52719be8db7b4c7189a1
nimishjn <nimish@electron.dev>
2025-10-30 16:32:53 +0530

    fix: about me

--- src/utils/constants/About.tsx
+++ src/utils/constants/About.tsx
@@ -1,10 +1,8 @@
 export const aboutContent = `
-I am a full stack developer with over one year of experience in **ReactJs**, **NextJs** and **NodeJs**.
-I have a passion for building software that is elegant, simple, and functional.
+Ambitious Full-Stack Software Engineer with experience architecting and launching high-performance web applications using Next.js, React, and Node.js. 
+Proven ability to deliver scalable solutions, dynamic content, optimize UX, amazing SEO, and drive millions of dollars  in transaction volume.
 I am not a one-trick programmer. I can build anything from the ground up and have a keen eye for design.
 If you have an idea, I have a web solution for you.
-I've also had conference papers and book chapters published in IEEE Xplore, Elsevier, and Springer as a **researcher**.
-I enjoy music, binge-watching Netflix, and playing basketball.
 
-I am working full-time as a SDE at **[Luganodes](https://luganodes.com/)**. If you have a project that you want to get started, think you need my help with something, or just fancy saying hello, then **[get in touch](#contact)**.
+I am working full-time as a Founding Frontend Engineer at **[Electron](https://www.electron.dev/)**. If you have a project that you want to get started, think you need my help with something, or just fancy saying hello, then **[get in touch](#contact)**.
 `;
